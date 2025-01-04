0720a317daec5b800c6e536f8e2de5ca32fd927c
Matheus Arakaki <arakakimath@gmail.com>
2025-01-04 12:46:07 -0400

    passwordHash, zodValidationPipe, clientHttpFile

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,13 +1,31 @@
-import { ConflictException, Body, Controller, Post } from '@nestjs/common'
+import {
+  ConflictException,
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+} from '@nestjs/common'
+import { hash } from 'bcryptjs'
+import { ZodValidationPipe } from 'src/pipes/zod-validation.pipe'
 
 import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+})
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
 
 @Controller('/accounts')
 export class CreateAccountController {
   constructor(private prisma: PrismaService) {}
 
   @Post()
-  async handle(@Body() body: any) {
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountBodySchema) {
     const { name, email, password } = body
 
     const userWithSameEmail = await this.prisma.user.findUnique({
@@ -15,6 +33,8 @@ export class CreateAccountController {
         email,
       },
     })
+
+    const hashedPassword = await hash(password, 8)
 
     if (userWithSameEmail) {
       throw new ConflictException(
@@ -26,7 +46,7 @@ export class CreateAccountController {
       data: {
         name,
         email,
-        password,
+        password: hashedPassword,
       },
     })
   }
