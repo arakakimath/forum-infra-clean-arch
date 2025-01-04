34644ac950eee6e6738a715944c1cc636c1e8159
Matheus Arakaki <arakakimath@gmail.com>
2025-01-04 13:11:48 -0400

    zod validation for environmental variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
+import { Env } from './env'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3333)
+  const app = await NestFactory.create(AppModule, {
+    // logger: false,
+  })
+
+  // Defining true in the second parameter to guarantee to nest that environmental variables were validated
+  // or else, 'port' is typed as 'number | undefined'
+  const configService = app.get<ConfigService<Env, true>>(ConfigService)
+  // const configService: ConfigService<Env> = app.get
+
+  const port = configService.get('PORT', { infer: true })
+
+  await app.listen(port)
 }
 bootstrap()
