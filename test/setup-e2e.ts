4786ecfb1d1d9820a07a74c207d4bb93edf8d1b2
Matheus Arakaki <arakakimath@gmail.com>
2025-01-08 13:26:22 -0400

    create-account-controller e2e test

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -5,11 +5,11 @@ import { execSync } from 'node:child_process'
 
 const prisma = new PrismaClient()
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('Please, provide a DATABASE_URL environment variable.')
-}
-
 function generateUniqueDatabaseURL(schemaId: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please, provide a DATABASE_URL environment variable.')
+  }
+
   const url = new URL(process.env.DATABASE_URL)
 
   url.searchParams.set('schema', schemaId)
