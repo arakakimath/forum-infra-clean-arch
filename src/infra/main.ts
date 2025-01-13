a1fa705c2d529c858531b4153b185a50fe13e057
Matheus Arakaki <arakakimath@gmail.com>
2025-01-13 15:53:05 -0400

    creating envModule

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { ConfigService } from '@nestjs/config'
-import { Env } from './env'
+import { EnvService } from './env/env.service'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -10,10 +9,11 @@ async function bootstrap() {
 
   // Defining true in the second parameter to guarantee to nest that environmental variables were validated
   // or else, 'port' is typed as 'number | undefined'
-  const configService = app.get<ConfigService<Env, true>>(ConfigService)
+  // const configService = app.get<ConfigService<Env, true>>(ConfigService)
   // const configService: ConfigService<Env> = app.get
 
-  const port = configService.get('PORT', { infer: true })
+  const envService = app.get(EnvService)
+  const port = envService.get('PORT')
 
   await app.listen(port)
 }
