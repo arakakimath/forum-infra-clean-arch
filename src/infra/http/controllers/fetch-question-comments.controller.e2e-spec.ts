10c3cfddedee11d1739990fed05b14e3f8a1ce6d
Matheus Arakaki <arakakimath@gmail.com>
2025-01-28 16:09:31 -0400

    QuestionCommentWithAuthor prisma, presenter and controller

--- src/infra/http/controllers/fetch-question-comments.controller.e2e-spec.ts
+++ src/infra/http/controllers/fetch-question-comments.controller.e2e-spec.ts
@@ -31,7 +31,9 @@ describe('Fetch question comments (E2E)', () => {
     await app.init()
   })
   test('[GET] /questions/:questionId/comments', async () => {
-    const user = await studentFactory.makePrismaStudent()
+    const user = await studentFactory.makePrismaStudent({
+      name: 'John Doe',
+    })
     const accessToken = jwt.sign({ sub: user.id.toString() })
     const question = await questionFactory.makePrismaQuestion({
       authorId: user.id,
@@ -61,8 +63,14 @@ describe('Fetch question comments (E2E)', () => {
 
     expect(response.body).toEqual({
       comments: expect.arrayContaining([
-        expect.objectContaining({ content: 'Comment 01' }),
-        expect.objectContaining({ content: 'Comment 01' }),
+        expect.objectContaining({
+          content: 'Comment 01',
+          authorName: 'John Doe',
+        }),
+        expect.objectContaining({
+          content: 'Comment 02',
+          authorName: 'John Doe',
+        }),
       ]),
     })
   })
