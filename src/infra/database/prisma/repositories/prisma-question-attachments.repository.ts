52af0e6ab94ac91899aa4ae007650489dc0791e9
Matheus Arakaki <arakakimath@gmail.com>
2025-01-28 11:15:06 -0400

    updating prisma methods to deal with attachments

--- src/infra/database/prisma/repositories/prisma-question-attachments.repository.ts
+++ src/infra/database/prisma/repositories/prisma-question-attachments.repository.ts
@@ -10,6 +10,30 @@ export class PrismaQuestionAttachmentsRepository
 {
   constructor(private prisma: PrismaService) {}
 
+  async createMany(attachments: QuestionAttachment[]): Promise<void> {
+    if (attachments.length === 0) return void
+    
+    const data = PrismaQuestionAttachmentMapper.toPrismaUpdateMany(attachments)
+
+    await this.prisma.attachment.updateMany(data)
+  }
+
+  async deleteMany(attachments: QuestionAttachment[]): Promise<void> {
+    if (attachments.length === 0) return void
+
+    const attachmentIds = attachments.map((attachment) => {
+      return attachment.id.toString()
+    })
+
+    await this.prisma.attachment.deleteMany({
+      where: {
+        id: {
+          in: attachmentIds,
+        }
+      }
+    })
+  }
+
   async findManyByQuestionId(
     questionId: string,
   ): Promise<QuestionAttachment[]> {
